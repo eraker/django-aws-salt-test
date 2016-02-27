913691561aa24ebdb5d926006f249ea3312cfbba
eraker <eraker@gmail.com>
2016-02-27 09:46:40 -0800

    pull app things out of prep salt

--- vagrant/prep_salt.sh
+++ vagrant/prep_salt.sh
@@ -4,10 +4,4 @@ sh install_salt.sh
 echo "file_client: local" >> /etc/salt/minion
 mkdir /srv/salt
 cp -r /opt/saltings/* /srv/salt
-pip install awscli --ignore-installed six
-aws s3 cp --recursive s3://ekadanta/ .
-mv ./secrets.yaml /opt/secrets.yaml
-export SECRETS_LOCATION=/opt/secrets.yaml
 salt-call --local state.highstate -l debug
-
-# Need to start application
